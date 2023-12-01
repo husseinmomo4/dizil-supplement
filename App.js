a420e7dfb0097c3b77563c986fa2f8b526155bd3
husseinmomo4 <146499139+husseinmomo4@users.noreply.github.com>
2023-12-01 04:50:35 +0200

    Update App.js

--- App.js
+++ App.js
@@ -5,80 +5,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { Ionicons } from "@expo/vector-icons";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './src/screens/HomeScreen';
 import Cart from './src/screens/Cart';
-import You from './src/screens/You';
+import You from './src/screens/You'
 import { FontAwesome5 } from '@expo/vector-icons';
-import ProdactInfo from './src/componants/ProdactInfo';
 import { Fontisto } from '@expo/vector-icons';
 import Catagor from './src/Screen2/Catagor';
 import CatagoriDetails from './src/Screen2/CatagoriDetails';
 import CatagoriPadg from './src/Screen2/CatagoriPadg';
 import Catagoris from './src/Screen2/Catagoris';
 
-
-
 const App = () => {
 const Tab = createBottomTabNavigator(); 
 const Stack = createNativeStackNavigator();
-
-    function BottomTabs () {
-  return (
-    
-        <Tab.Navigator screenOptions={{
-
-          headerShown: false,
-
-      }}>
-        <Tab.Screen 
-          name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: "Home",
-            tabBarLabelStyle: { color: "Black"},
-            tabBarIcon: ({ focused }) =>
-              focused ? (
-                <Entypo name="home" size={24} color='#84dbc8' />
-              ) : (
-                <AntDesign name="home" size={24} color="Black" />
-              ),
-          }}
-        />
-
-
-        <Tab.Screen
-          name="You"
-          component={You}
-          options={{
-            tabBarLabel: "You",
-            tabBarLabelStyle: { color: "Black" },
-            tabBarIcon: ({ focused }) =>
-                   focused ? (
-       <FontAwesome5 name="user-alt" size={24} color='#84dbc8'/>
-                       ) : (
-       <FontAwesome5 name="user" size={26} color="black" />                ),
-                 }}
-               /> 
-
-
-              <Tab.Screen 
-                 name="Cart"
-                 component={Cart}
-                 options={{
-                   tabBarLabel: "Create",
-                   tabBarLabelStyle: { color: "Black"},
-                   tabBarIcon: ({ focused }) =>
-             focused ? (
-              <Ionicons name="cart" size={26} color='#84dbc8' />
-              ) : (
-                <Ionicons name="cart-outline" size={28} color="black" />
-                ),
-              }}
-        />
-      </Tab.Navigator>
-      )
-    }
-
 
     function Supplements () {
       return (
@@ -116,9 +54,6 @@ const Stack = createNativeStackNavigator();
               }}
             />
             
-            
-    
-    
             <Tab.Screen
               name="You"
               component={You}
@@ -132,8 +67,7 @@ const Stack = createNativeStackNavigator();
            <FontAwesome5 name="user" size={26} color="black" />                ),
                      }}
                    /> 
-    
-    
+
                   <Tab.Screen 
                      name="Cart"
                      component={Cart}
@@ -153,7 +87,6 @@ const Stack = createNativeStackNavigator();
         }
 
       return (
-      
         <>
         <StatusBar  style="light" />
       <NavigationContainer>
@@ -164,14 +97,9 @@ const Stack = createNativeStackNavigator();
           <Stack.Screen name="CatagoriDetails" component={CatagoriDetails} />
           <Stack.Screen name="CatagoriPadg" component={CatagoriPadg} />
           <Stack.Screen name="Catagoris" component={Catagoris} />
-          <Stack.Screen name="HomeScreen" component={HomeScreen} />
-          <Stack.Screen name="ProdactInfo" component={ProdactInfo} />
-
-
         </Stack.Navigator>
       </NavigationContainer>
     </>
   );
 }
-
 export default App
